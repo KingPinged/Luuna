bbd494f3f30fba424218e813c8b88246a9528a58
KingPing <kingsinland@gmail.com>
2023-02-06 16:26:19 -0600

    starter init file for Knit now fixed removed "controller" when should be "service" + changed project.json to account for file rename

--- src/server/starter.server.lua
+++ src/server/starter.server.lua
@@ -2,8 +2,6 @@ local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local config = require(ReplicatedStorage:WaitForChild("config"))
 
-Knit.AddControllers(script.Parent:WaitForChild("SharedClient").controllers)
-
 if game.PlaceId == config.lobbyPlaceId then
 	Knit.AddServices(script.Parent.Lobby.services)
 end
