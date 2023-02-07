68dea578d7b4cec4a27291ce2e7ed462e71162af
KingPing <kingsinland@gmail.com>
2023-02-06 18:54:03 -0600

    TeleportService can now send the player to game place

--- src/server/Lobby/services/TeleportService.lua
+++ src/server/Lobby/services/TeleportService.lua
@@ -1,14 +1,46 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RobloxTeleportService = game:GetService("TeleportService")
+
+local config = require(ReplicatedStorage.config)
+
+local Promise = require(ReplicatedStorage.Packages.Promise)
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
 local TeleportService = Knit.CreateService({
 	Name = "TeleportService",
-	Client = {},
+	Client = {
+		playerTeleported = Knit.CreateSignal(),
+	},
 })
+
+function TeleportService:TeleportPlayer(player)
+	Promise.retry(
+		Promise.new(function(resolve, reject)
+			-- do something that can fail
+
+			local success, result = pcall(function()
+				return RobloxTeleportService:TeleportAsync(config.gamePLaceId, { player })
+			end)
+
+			if not success then
+				warn("Teleport failed: " .. result)
+				reject(result)
+			else
+				resolve(result)
+			end
+		end),
+		config.maxTeleportTries
+	)
+end
 
 function TeleportService:KnitStart() end
 
-function TeleportService:KnitInit() end
+function TeleportService:KnitInit()
+	self.Client.playerTeleported:Connect(function(player)
+		print(`{player.Name} teleport request`)
+		self:TeleportPlayer(player)
+	end)
+end
 
 return TeleportService
--- src/shared/config.lua
+++ src/shared/config.lua
@@ -1,4 +1,6 @@
 return {
 	lobbyPlaceId = 12370590123,
+	gamePLaceId = 12370961507,
+	maxTeleportTries = 10,
 	startingTime = 1674542319, -- the time that the custom calendar starts at in Unix epoch format
 }
