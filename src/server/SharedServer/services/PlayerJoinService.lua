b78a68d58c8d383046eb00a57b004643cb0bc21b
KingPing <55996207+KingPinged@users.noreply.github.com>
2023-02-07 15:27:56 +0000

    feat: add player join service

---
+++ src/server/SharedServer/services/PlayerJoinService.lua
@@ -0,0 +1,39 @@
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+local PlayerJoinService = Knit.CreateService {
+    Name = "PlayerJoinService",
+    Client = {},
+}
+
+local PlayerService = game:GetService("Players")
+
+--todo does getting a service require it to be loaded by lifecycle first? 
+local ModeratorService 
+
+local players = {}
+
+--todo maybe not use player as index but id?
+function PlayerJoined(player)
+    players[player] = player
+
+
+end
+
+function PlayerJoinService:KnitStart()
+    
+end
+
+
+function PlayerJoinService:KnitInit()
+    ModeratorService = Knit.GetService("ModeratorService")
+    PlayerService.PlayerAdded:Connect(function(player)
+        PlayerJoined(player)
+    end)
+
+     for _, player in pairs(PlayerService:GetPlayers()) do
+        PlayerJoined(player)
+    end
+end
+
+
+return PlayerJoinService
