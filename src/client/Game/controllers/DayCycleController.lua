bd7368928e63d5265ebbcaaf12752bb907319170
KingPing <55996207+KingPinged@users.noreply.github.com>
2023-02-09 16:24:33 +0000

    feat: daycycle controller

---
+++ src/client/Game/controllers/DayCycleController.lua
@@ -0,0 +1,11 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+local DayCycleController = Knit.CreateController({ Name = "DayCycleController" })
+
+function DayCycleController:KnitStart() end
+
+function DayCycleController:KnitInit() end
+
+return DayCycleController
