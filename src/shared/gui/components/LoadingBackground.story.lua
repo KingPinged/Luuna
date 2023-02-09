9fc451a10749b279a004ae14ccb9a61cf70c810f
KingPing <55996207+KingPinged@users.noreply.github.com>
2023-02-09 16:17:40 +0000

    Feat: LoadingBackground react component

---
+++ src/shared/gui/components/LoadingBackground.story.lua
@@ -0,0 +1,24 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Roact = require(ReplicatedStorage.Packages.React)
+local ReactRoblox = require(ReplicatedStorage.Packages.ReactRoblox)
+
+local e = Roact.createElement
+
+local BackgroundComponent = require(script.parent.LoadingBackground)
+
+local controls = {
+	duration = 30,
+}
+
+return {
+	summary = "The backgound that appears in game loading and teleport screen",
+	react = Roact,
+	controls = controls,
+	reactRoblox = ReactRoblox,
+	story = function(props)
+		return e(BackgroundComponent, {
+			duration = props.controls.duration,
+		})
+	end,
+}
