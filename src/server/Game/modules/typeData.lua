dd8334f386325c8b08f457f7c7965bc16e2064d3
KingPing <55996207+KingPinged@users.noreply.github.com>
2023-02-15 15:57:17 +0000

    feat: types now uses enumlist

--- src/server/Game/modules/typeData.lua
+++ src/server/Game/modules/typeData.lua
@@ -1,6 +1,37 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local EnumList = require(ReplicatedStorage.Packages.EnumList)
+
+local types = EnumList.new("Types", {
+	"fire",
+	"water",
+	"ground",
+	"earth",
+    "nature"
+})
+
+
+
 return {
-    "fire" = {
-        
-    }
+    types.fire = {
+        "weak" = type.water,
+        "strong" = type.nature,
+    },
+    types.water = {
+        "weak" = type.ground,
+        "strong" = type.earth,
+    },
+    types.ground = {
+        "weak" = type.ground,
+        "strong" = type.ground,
+    },
+    types.earth = {
+        "weak" = type.earth,
+        "strong" = type.earth,
+    },
+    types.nature = {
+        "weak" = type.nature,
+        "strong" = type.nature,
+    },
 
 }
