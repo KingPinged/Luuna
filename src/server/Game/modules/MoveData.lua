29d5b41768b0ddfb91f81ace80afbd91de30317a
KingPing <55996207+KingPinged@users.noreply.github.com>
2023-02-16 18:43:43 +0000

    feat: battle and data update

--- src/server/Game/modules/MoveData.lua
+++ src/server/Game/modules/MoveData.lua
@@ -1,13 +1,43 @@
 return {
     "scratch" = {
-
+        "name" = "Scratch",
+        "type" = "normal",
+        "category" = "physical",
+        "power" = 40,
+        "accuracy" = 100,
+        "pp" = 35,
+        "priority" = 0,
+        "target" = "normal",
+        "flags" = {
+            "contact",
+            "protect",
+            "mirror"
+        },
+        "secondary" = {
+            "chance" = 10,
+            "status" = "flinch"
+        },
+        "self" = {
+            "boosts" = {
+                "atk" = 1
+            }
+        },
+        "desc" = "A scratch attack that may also cause flinching."
+    },
     },
     "flap" = {
 
 
     },
-    "test" = {
-
+    "bulk up" = {
+        "name" = "Bulk Up",
+        "type" = "normal",
+        "category" = "secondary",
+        "power" = 40,
+        "accuracy" = 100,
+        "pp" = 35,
+        "priority" = 0,
+        
     },
     "test2" = {
 
