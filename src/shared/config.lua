cb7ebcdc5588c3ff785ef6134eebab7928bd1d8d
KingPing <kingsinland@gmail.com>
2023-03-21 22:38:51 -0500

    ref: gamePlaceId syntax error

--- src/shared/config.lua
+++ src/shared/config.lua
@@ -1,7 +1,7 @@
 return {
 	version = "0.0.1", -- the version of the game
 	lobbyPlaceId = 12370590123,
-	gamePLaceId = 12370961507,
+	gamePlaceId = 12370961507,
 	maxTeleportTries = 10,
 	maxModerationTries = 20,
 	startingTime = 1674542319, -- the time that the custom calendar starts at in Unix epoch format
